357778cd5992331c42fa19e85df34c791a51aab5
hotcream3904 <130561236+hotcream3904@users.noreply.github.com>
2025-07-09 22:20:03 +0900

    chore:JN-7 FSD 폴더구조 세팅

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {}
+
+const MyComponent: React.FC<Props> = () => {
+  return (
+    <div>
+      MyComponent
+    </div>
+  );
+};
+
+export default MyComponent;
